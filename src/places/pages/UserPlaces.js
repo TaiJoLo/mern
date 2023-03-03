f98c6709b30fe988b612005b3ea0e5b63b2c3acf
Jerboa <lotaijo@gmail.com>
2023-03-03 17:55:49 +1300

    rendering user places & using dynamic route segments

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://images.unsplash.com/photo-1620400557579-93560a77987c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://images.unsplash.com/photo-1620400557579-93560a77987c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80",
+
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
